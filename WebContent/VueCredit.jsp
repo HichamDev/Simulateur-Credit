331a12cb799ac53f7293517d464b57ffe305e5fe
HichamDev <daha.hicham@gmail.com>
2017-09-22 13:00:41 +0100

    add 2eme commit

--- WebContent/VueCredit.jsp
+++ WebContent/VueCredit.jsp
@@ -1,10 +1,52 @@
+
+<%@page import="web.CreditModel"%>
+<%
+	CreditModel mod=(CreditModel)request.getAttribute("mod");
+%>
 <!DOCTYPE html >
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Simulateur Crédit</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
- <h3>Test Crédit :</h3>
+ <div>
+ 	<form action="controleur.php" method="post">
+ 		<table>
+ 			<tr>
+ 				<td>Montant : </td>
+ 				<td><input type="text" name="montant" value="<%=mod.getMontant()%>" /></td>
+ 				<td>DH</td>
+ 			</tr>
+ 			<tr>
+ 				<td>Durée : </td>
+ 				<td><input type="text" name="duree" value="<%=mod.getDuree()%>"/></td>
+ 				<td>Mois</td>
+ 			</tr>
+ 			<tr>
+ 				<td>Taux : </td>
+ 				<td><input type="text" name="taux" value="<%=mod.getTaux()%>"/></td>
+ 				<td>%</td>
+ 			</tr>
+ 			<tr>
+ 				
+ 				<td><input type="submit" name="Calculer"/></td>
+ 				
+ 			</tr>
+ 		
+ 		</table>	
+ 	</form>
+ </div>
+ 
+ <div>
+ 	<table>
+ 		<tr>
+ 			<td>Mensualité:</td>
+ 			<td><%=mod.getMensualite() %> </td>
+ 			<td>DH</td>
+ 		</tr>
+ 	</table>
+ </div>
 </body>
 </html>
--- src/web/ControleurServlet.java
+++ src/web/ControleurServlet.java
@@ -3,12 +3,13 @@ package web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import metier.CreditMetir;
-
+@WebServlet(name="cs", urlPatterns={"/controleur" , "*.php"})
 public class ControleurServlet extends HttpServlet{
 	
 	private CreditMetir metier;
@@ -23,9 +24,29 @@ public class ControleurServlet extends HttpServlet{
 		HttpServletResponse resp) 
 		throws ServletException, IOException {
 		
+		req.setAttribute("mod", new CreditModel());
 		req.getRequestDispatcher("VueCredit.jsp").
 		forward(req, resp);
 		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		double montant=Double.parseDouble(req.getParameter("montant"));
+		double taux=Double.parseDouble(req.getParameter("taux"));
+		int duree=Integer.parseInt(req.getParameter("duree"));
+		
+		CreditModel model = new CreditModel();
+		model.setMontant(montant);
+		model.setTaux(taux);
+		model.setDuree(duree);
+		
+		double res = metier.calculMensualite(montant, duree, taux);
+		
+		model.setMensualite(res);
+		
+		req.setAttribute("mod", model);
+		req.getRequestDispatcher("VueCredit.jsp").forward(req, resp);
 	}
 
 }
